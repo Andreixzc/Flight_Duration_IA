ba543429e73cf24dfd4563115dc793a73a864da4
Andrei <andrei.massaini@hotmail.com>
2024-08-02 13:54:58 -0300

    coments

--- RetriveSorted.py
+++ RetriveSorted.py
@@ -2,13 +2,11 @@ import sqlite3
 import pandas as pd
 #Script que puxa do banco todos os voos de cada aeronave e salva cada um em um arquivo.
 
-# Caminho para o banco de dados
+
 db_file = 'Database/Flights.db'
 
-# Conecta ao banco de dados
 conn = sqlite3.connect(db_file)
 
-# Cria um cursor para executar comandos SQL
 cursor = conn.cursor()
 
 # Executa a consulta SQL para selecionar todos os dados da tabela positions, ordenando pelo IdAircraft
@@ -17,13 +15,11 @@ SELECT * FROM positions
 ORDER BY IdAircraft
 '''
 
-# Usa pandas para ler o resultado da consulta SQL diretamente para um DataFrame
 positions_df = pd.read_sql_query(query, conn)
 
-# Fecha a conexão com o banco de dados
 conn.close()
 
-# Salva o DataFrame em arquivos CSV separados por IdAircraft
+#DataFrame em arquivos CSV separados por IdAircraft
 unique_aircraft_ids = positions_df['IdAircraft'].unique()
 
 for aircraft_id in unique_aircraft_ids:
